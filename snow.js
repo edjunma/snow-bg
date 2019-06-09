c52469c6f8a9c4f367dba30f674ae4487f414bb3
edjunma <junwenma@buffalo.edu>
2019-06-09 12:33:32 -0400

    first commit, boilerplate and functions

---
+++ snow.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+	// get the canvas and context and store in vars
+	var canvas = document.getElementById('sky');
+  var ctx = canvas.getContext('2d');
+  
+  // set canvas dims to window height and width
+};
