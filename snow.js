95fb9e2f8fd23d13059364412598dad4d32e46f5
edjunma <junwenma@buffalo.edu>
2019-06-09 12:52:50 -0400

    finish snow bg with intervals

--- snow.js
+++ snow.js
@@ -11,49 +11,50 @@ window.onload = function() {
 
 	// generate the snowflakes and apply attributes
 	var mf = 100; //,ax amount of flakes
-  var flakes = [];
-  
-  // loop through the empty flakes and apply attributes
-  for(var i = 0; i < mf; i++){
-    flakes.push({
-      x = Math.random()*W,
-      y = Math.random() * H,
-      r = Math.random() * 5 + 2, //min of 2px and max of 7px
-      d = Math.random() + 1 //density of the flake
-    })
-  }
+	var flakes = [];
 
-  // draw flakes onto canvas
-  function drawflakes() {
-    ctx.clearRect(0, 0, W, H);
-    ctx.fillStyle = "white";
-    ctx.beginPath();
-    for(var i = 0; i < mf; i++){
-      var f = flakes[i];
-      ctx.moveTo(f.x, f.y);
-      ctx.arc(f.x, f.y, f.r, 0, Math.PI*2, true);
-    }
-    ctx.fill();
-    moveFlakes();
-  }
+	// loop through the empty flakes and apply attributes
+	for (var i = 0; i < mf; i++) {
+		flakes.push({
+			x: Math.random() * W,
+			y: Math.random() * H,
+			r: Math.random() * 5 + 2, //min of 2px and max of 7px
+			d: Math.random() + 1 //density of the flake
+		});
+	}
 
-  // animate the flakes
-  var angle = 0;
+	// draw flakes onto canvas
+	function drawFlakes() {
+		ctx.clearRect(0, 0, W, H);
+		ctx.fillStyle = 'white';
+		ctx.beginPath();
+		for (var i = 0; i < mf; i++) {
+			var f = flakes[i];
+			ctx.moveTo(f.x, f.y);
+			ctx.arc(f.x, f.y, f.r, 0, Math.PI * 2, true);
+		}
+		ctx.fill();
+		moveFlakes();
+	}
 
-  function moveFlakes() {
-    angle += 0.01;
-    for(var i = 0; i <mf; i++){
-      // store current flake
-      var f = flakes[i];
+	// animate the flakes
+	var angle = 0;
 
-      // update X and Y coordinates of each snowflake
-      f.y += Math.pow(f.d, 2) + 1;
-      f.x += Math.sin(angle) * 2;
+	function moveFlakes() {
+		angle += 0.01;
+		for (var i = 0; i < mf; i++) {
+			// store current flake
+			var f = flakes[i];
 
-      // if the snowflakes reaches the bottom, send a new one to the top
-      if(f.y > H) {
-        flakes[i] = {x: Math.random()*W, y:0, r:0, r: f.r, d: f.d};
-      }
-    }
-  }
+			// update X and Y coordinates of each snowflake
+			f.y += Math.pow(f.d, 2) + 1;
+			f.x += Math.sin(angle) * 2;
+
+			// if the snowflakes reaches the bottom, send a new one to the top
+			if (f.y > H) {
+				flakes[i] = { x: Math.random() * W, y: 0, r: f.r, d: f.d };
+			}
+		}
+	}
+	setInterval(drawFlakes, 25);
 };
